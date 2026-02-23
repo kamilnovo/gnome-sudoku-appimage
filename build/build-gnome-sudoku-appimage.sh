ef7eb5ead4581b12c7ce6c00e850312ffce5f45c
kamilnovo <kamilnovo@seznam.cz>
2026-02-23 20:21:22 +0100

    Switch back to ubuntu-24.04 and use native libraries with patching

--- build/build-gnome-sudoku-appimage.sh
+++ build/build-gnome-sudoku-appimage.sh
@@ -8,50 +8,49 @@ APPDIR="AppDir"
 
 cd "$(dirname "$0")/.."
 REPO_ROOT="$PWD"
-echo "Repo root: $REPO_ROOT"
 
-rm -rf "$APPDIR" "$PROJECT_DIR" blueprint-dest gtk-dest
+rm -rf "$APPDIR" "$PROJECT_DIR"
 mkdir -p "$APPDIR"
 
-# Helper for building deps
-build_dep() {
-    local name=$1 url=$2 version=$3 dest=$4
-    echo "=== Building $name $version ==-"
-    git clone --depth 1 --branch "$version" "$url" "$name-src"
-    cd "$name-src"
-    meson setup build --prefix=/usr --libdir=lib
-    DESTDIR="$dest" meson install -C build
-    export PKG_CONFIG_PATH="$dest/usr/lib/pkgconfig:$PKG_CONFIG_PATH"
-    export LD_LIBRARY_PATH="$dest/usr/lib:$LD_LIBRARY_PATH"
-    export PATH="$dest/usr/bin:$PATH"
-    cd "$REPO_ROOT"
-}
-
-# 1. Build dependencies from source
-build_dep "blueprint-compiler" "https://gitlab.gnome.org/jwestman/blueprint-compiler.git" "v0.16.0" "$REPO_ROOT/blueprint-dest"
-build_dep "gtk" "https://gitlab.gnome.org/GNOME/gtk.git" "4.14.4" "$REPO_ROOT/gtk-dest"
-build_dep "libadwaita" "https://gitlab.gnome.org/GNOME/libadwaita.git" "v1.5.0" "$REPO_ROOT/gtk-dest"
-
-# 2. Build gnome-sudoku
-echo "=== Fetching gnome-sudoku $VERSION ==-"
+# 1. Fetch source
 git clone --depth 1 --branch "$VERSION" "$REPO_URL" "$PROJECT_DIR"
+
+# 2. Patch for Ubuntu 24.04 libraries (GLib 2.80, GTK 4.14)
+# Sudoku 49.x might want GLib 2.82 or GTK 4.18, let's lower it.
+sed -i "s/glib_version = '2.82.0'/glib_version = '2.80.0'/g" "$PROJECT_DIR/meson.build" || true
+sed -i "s/gtk4', version: '>= 4.18.0'/gtk4', version: '>= 4.14.0'/g" "$PROJECT_DIR/meson.build" || true
+sed -i "s/libadwaita-1', version: '>= 1.7'/libadwaita-1', version: '>= 1.5'/g" "$PROJECT_DIR/meson.build" || true
+
+# 3. Build
 cd "$PROJECT_DIR"
-echo "=== Building gnome-sudoku ==-"
 meson setup build --prefix=/usr -Dbuildtype=release
 meson compile -C build
 DESTDIR="$REPO_ROOT/$APPDIR" meson install -C build
 cd "$REPO_ROOT"
 
-# 3. Bundle everything with linuxdeploy
+# 4. Handle GIO and GSettings
+mkdir -p "$APPDIR/usr/lib/x86_64-linux-gnu/gio/modules"
+cp /usr/lib/x86_64-linux-gnu/gio/modules/libdconfsettings.so "$APPDIR/usr/lib/x86_64-linux-gnu/gio/modules/" || true
+cp /usr/lib/x86_64-linux-gnu/gio/modules/libgiognutls.so "$APPDIR/usr/lib/x86_64-linux-gnu/gio/modules/" || true
+
+# Compile schemas
+glib-compile-schemas "$APPDIR/usr/share/glib-2.0/schemas"
+
+# 5. Packaging
 wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -O linuxdeploy
 wget -q https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh -O linuxdeploy-plugin-gtk.sh
 chmod +x linuxdeploy linuxdeploy-plugin-gtk.sh
 
-# Copy built libs to AppDir
-cp -a "$REPO_ROOT/gtk-dest/usr/lib"/* "$APPDIR/usr/lib/" 2>/dev/null || true
+# Find desktop and icon
+DESKTOP_FILE=$(find "$APPDIR" -name "org.gnome.Sudoku.desktop")
+ICON_FILE=$(find "$APPDIR" -name "org.gnome.Sudoku.svg" | grep -v "symbolic" | head -n 1)
 
 export VERSION
 ./linuxdeploy --appdir "$APPDIR" \
     -e "$APPDIR/usr/bin/gnome-sudoku" \
+    ${DESKTOP_FILE:+ -d "$DESKTOP_FILE"} \
+    ${ICON_FILE:+ -i "$ICON_FILE"} \
     --plugin gtk \
     --output appimage
+
+echo "Done!"
