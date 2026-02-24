cb7bed936a41ea59b536bb92e811e7d7cafbbc5b
kamilnovo <kamilnovo@seznam.cz>
2026-02-24 06:31:27 +0100

    Comment out set_accent_color body for compatibility

--- build/build-gnome-sudoku-appimage.sh
+++ build/build-gnome-sudoku-appimage.sh
@@ -58,8 +58,8 @@ sed -i '/focus-widget:/d' "$PROJECT_DIR/src/blueprints/print-dialog.blp" || true
 
 # Patch Vala code for Libadwaita 1.5 compatibility
 # Adw.StyleManager.get_accent_color() and Adw.AccentColor are 1.6+
-# We insert an early return to bypass the entire function body safely
-sed -i '/void set_accent_color ()/!b;n;a \        return;' "$PROJECT_DIR/src/window.vala" || true
+# We use a robust way to comment out the body of the function
+sed -i '/void set_accent_color ()/,/}/ { /void set_accent_color ()/b; /}/b; s/^/\/\// }' "$PROJECT_DIR/src/window.vala" || true
 
 # 3. Build
 cd "$PROJECT_DIR"
